9dca6a8122d84ebbc07b5f6f9a3df4dfd7cbd882
uzzinni <yujin7154@gmail.com>
2025-02-28 09:52:06 +0900

    [MOD] Spring 기초 day1 수정 후 최종본

--- feb27/src/main/java/com/poseidon/web/HomeController.java
+++ feb27/src/main/java/com/poseidon/web/HomeController.java
@@ -33,21 +33,11 @@ public class HomeController {
 	@Autowired
 	private BoardService boardService;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "home"; // home.jsp
 	}
+	
 	//@RequestMapping(value = "/board" , method = RequestMethod.GET)
 	@GetMapping("/board")
 	public String board(Model model) {
