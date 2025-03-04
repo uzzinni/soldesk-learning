91721a22175af9d6fd263b35d36df62b1d3ecca4
uzzinni <yujin7154@gmail.com>
2025-03-04 22:18:28 +0900

    [MOD] Spring 기초 day1 수정 후 최종본

--- feb27/src/main/java/com/poseidon/web/HomeController.java
+++ feb27/src/main/java/com/poseidon/web/HomeController.java
@@ -1,15 +1,11 @@
 package com.poseidon.web;
 
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
