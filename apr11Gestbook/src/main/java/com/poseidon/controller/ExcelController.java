e529b8c8423f49119a1159b972ea10b1f4f568ba
uzzinni <yujin7154@gmail.com>
2025-04-22 21:12:39 +0900

    [FEAT] Excel 업로드 후 데이터 읽기 구현

--- apr11Gestbook/src/main/java/com/poseidon/controller/ExcelController.java
+++ apr11Gestbook/src/main/java/com/poseidon/controller/ExcelController.java
@@ -1,16 +1,9 @@
 package com.poseidon.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -60,46 +53,12 @@ public class ExcelController {
 
 	@PostMapping("/readExcel")
 	public String read(@RequestParam("file") MultipartFile upFile, Model model) throws IOException {
-		//엔티티 만들어주고 올게요. 나중에.
-		List<Map<String, Object>> data = new ArrayList<>();
+		//서비스로 이동해주세요.
+		List<Map<String, Object>> data = excelService.readExcel(upFile);
 		
-		Workbook workbook = new XSSFWorkbook(upFile.getInputStream());
-		Sheet sheet = workbook.getSheetAt(0);
-		Row titleRow = sheet.getRow(0);
-		
-		List<String> columnTitles = new ArrayList<String>(); //타이틀만 따로 뽑아서 저장하기
-		for (Cell cell : titleRow) {
-			columnTitles.add(cell.getStringCellValue());
-		}		
-		
-		for (int i = 1; i < sheet.getLastRowNum() ; i++) {
-			Row row = sheet.getRow(i);
-			Map<String, Object> dataMap = new HashMap<String, Object>();
-			
-			for (int j = 0; j < columnTitles.size(); j++) {
-				Cell cell = row.getCell(j);
-				Object value = null;
-				
-				if (cell != null) {
-					//System.out.println(cell.getCellType());
-					switch (cell.getCellType()) {
-					case STRING:
-						value = cell.getStringCellValue();
-						break;
-					case NUMERIC:
-						break;
-					case BOOLEAN:
-						break;
-					default:
-						value = null;
-					}
-				} //end if
-				dataMap.put(columnTitles.get(j), value);
-			} // end for J
-			data.add(dataMap);
-		}// end for I
-		System.out.println(data); //최종 출력
-		model.addAttribute("data", data);
+		//System.out.println(data); //최종 출력
+		excelService.readExcel(data); //서비스 통해서 JPA로 데이터베이스에 저장하기
+		model.addAttribute("data", data); // 화면에 출력
 		return "admin/upfile";
 	}
 	
---
+++ apr11Gestbook/src/main/java/com/poseidon/repository/IpTableReoisitory.java
@@ -0,0 +1,8 @@
+package com.poseidon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.poseidon.entitiy.Excel;
+
+public interface IpTableReoisitory extends JpaRepository<Excel, Integer> {
+
+}
