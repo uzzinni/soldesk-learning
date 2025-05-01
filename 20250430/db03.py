09c9704849c8c65328de810845ef7476c3f3bd03
uzzinni <yujin7154@gmail.com>
2025-05-01 20:16:26 +0900

    [FEAT] 점수 계산 구현 코드 추가

--- 20250430/db03.py
+++ 20250430/db03.py
@@ -1,5 +1,7 @@
 # 아까 복사했던 import 없앴어요. -> DBConn.py 파일로 만들어서 ...
 #import DBConn
+from sympy import totient
+
 from DBConn import DBConn
 
 # 변수 = input("번호를 입력하세요")
@@ -14,21 +16,44 @@ def list():
     cursor.execute(sql)
     result = cursor.fetchall()
     if result:
+        print('┌──────────┬────────────────────────────────────────┐')
+        print('│{0:^9}│\t'.format('이름'))
+        print('├──────────┼────────────────────────────────────────┤')
         for i in result:
-            print(i)
+            print('│{0:^9}│\t{1:3}'
+                  .format(i[0], i[1]))
+
     else:
         print("데이터가 없습니다.")
     cursor.close()
-
+    print('└──────────┴────────────────────────────────────────┘')
 
 # 2. 추가하는 함수
 def insert():
     print("데이터를 추가합니다.")
     name = input("이름을 입력하세요.")
-    kor = input("국어 점수를 입력하세요.")
-    eng = input("영어 점수를 입력하세요.")
-    mat = input("수학 점수를 입력하세요.")
+    kor = int(input("국어 점수를 입력하세요."))
+    eng = int(input("영어 점수를 입력하세요."))
+    mat = int(input("수학 점수를 입력하세요."))
     # 나머지 점수는 계산
+    tot = kor + eng + mat # 저 위에 input() 정수로 변환해주세요.
+    ave = tot / 3.0
+    # list만들어서 담기
+    data = [name, kor, eng, mat, tot, ave]
+
+    conn = DBConn.conn()
+    cursor = conn.cursor()
+    sql = ("INSERT INTO score (name, kor, eng, mat, tot, ave) "
+           "VALUES (%s, %s, %s, %s, %s, %s)")
+    cursor.execute(sql, data)
+    conn.commit()
+    conn.close()
+    print("입력 완료했습니다.")
+
+
+
+
+
 
 # 3. 삭제를 실행하는 함수
 def delete():
